ed26101afe9ed3a2078f21fe68aa4428edec06d9
Desolated <makaryurkov2003@gmail.com>
2023-10-19 19:42:57 +0300

    Добавьте файлы проекта.

---
+++ Logger_v2/Logger_v2.ps1
@@ -0,0 +1,81 @@
+﻿$conf_filename = "config.json"
+$script_filename = "mail_sender.ps1"
+$current_scriptname = "Logger_v2.ps1"
+$xml_filename = "StatusMailReporter.xml"
+
+#Рабочие папка текущего скрипта
+$current_directory = $MyInvocation.MyCommand.Path | split-path -parent
+$mod_directory = "$current_directory\mod"
+
+
+#Проверка прав администратора и запрос в случае отсутствия
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole( `
+[Security.Principal.WindowsBuiltInRole] "Administrator")) {
+	Start-Process Powershell -ArgumentList "$current_directory\$current_scriptname" -Verb RunAs -ErrorAction Ignore
+	exit
+}
+
+
+#Описание задачи
+$task_directory = "\"
+$task_name = "StatusMailReporter"
+$task_description = "Отправка сообщения SMTP о запуске системы или её выходе из спящего режима"
+$xml_filename = $task_name + ".xml"
+
+#Расположение скрипта для отправки почты и его конфигурационного файла в системе после распаковки!!!
+$dst_directory = "C:\ProgramData\" + $task_name
+
+
+#Почтовая конфигурация
+$stmp_server = ''
+$sender_mail = ''
+$app_password = ''
+$dest_mail = ''
+
+#Ввод отсутствующей почтовой конфигурации
+if ($stmp_server -eq ''){
+	$stmp_server = Read-Host "Enter STMP server"
+}
+if ($sender_mail -eq ''){
+	$sender_mail = Read-Host "Enter logsender mail"
+}
+if ($app_password -eq ''){
+	$app_password = Read-Host "Enter APP-password"
+}
+if ($dest_mail -eq ''){
+	$dest_mail = Read-Host "Enter the dest_email address"
+}
+
+
+
+#Проверка на существование задачи с данным именем и её дективация при нахождении
+if (Get-ScheduledTask $task_name -ErrorAction Ignore) {
+echo "Task will be overwritten"
+Unregister-ScheduledTask `
+-TaskPath $task_directory `
+-TaskName $task_name `
+-Confirm:$false
+}
+
+#Ввод имени компьютера, который будет отображаться в теме сообщения(при пустой строке тема будет только @)
+$computer_name = Read-Host "Enter the computer name(as you wish)"
+
+
+#Размещение скрипта и конфигурации
+if ($dst_directory)
+{
+	echo "Directory will be overwritten"
+}
+New-Item -ItemType Directory -Path $dst_directory -Force
+Copy-Item -Path "$mod_directory\$script_filename" -Destination "$dst_directory\$script_filename" -Force
+New-Item -Path "$dst_directory\$conf_filename" -ItemType File `
+-Value "
+{`"stmp_server`": `"$stmp_server`",
+  `"sender_mail`": `"$sender_mail`",
+  `"app_password`": `"$app_password`",
+  `"dest_mail`": `"$dest_mail`",
+  `"computer_name`": `"$computer_name`"}" -Force
+
+ 
+  #Регистрация XML файла как задачу  
+  Register-ScheduledTask -Xml (Get-Content "$mod_directory\$xml_filename" | out-string) -TaskName $task_name -TaskPath $task_directory
---
+++ Logger_v2/mod/mail_sender.ps1
@@ -0,0 +1,54 @@
+#Считывает параметры с файла, созданного при регистрации, отправляет сообщение на почту
+#
+#
+#
+#Получить настройки с конфигурационного файла json
+$directory = $MyInvocation.MyCommand.Path | split-path -parent
+$json_path = $directory + "\config.json"
+$Config = Get-Content -Path $json_path | ConvertFrom-Json
+if ($Config -eq $null)
+{
+	exit
+}
+$stmp_server = $Config.stmp_server
+$sender_mail = $Config.sender_mail
+$app_password = $Config.app_password
+$dest_mail = $Config.dest_mail
+$computer_name = $Config.computer_name
+if (($stmp_server -eq $null) -or ($sender_mail -eq $null) -or ($app_password -eq $null) `
+	-or ($dest_mail -eq $null) -or ($computer_name -eq $null) `
+-or ($stmp_server -eq "") -or ($sender_mail -eq "") -or ($app_password -eq "") `
+	-or ($dest_mail -eq ""))
+{
+	exit
+}
+
+
+#Настройка и отправка сообщения
+$CurrentDate = Get-Date -Format 'HH:mm dddd dd/MM/yyyy'
+$subject = "@" + $computer_name
+$Body = 'Компьютер включён в ' + $CurrentDate
+$Password = $app_password | ConvertTo-SecureString -AsPlainText -Force
+$Credential = New-Object `
+-TypeName System.Management.Automation.PSCredential `
+-ArgumentList $sender_mail, $Password
+#Отправка сообщения с ожиданием подключения к сети интернет
+while ($true)
+{
+	if ((Test-NetConnection -WarningAction SilentlyContinue).PingSucceeded)
+	{
+		Send-MailMessage `
+		-From $sender_mail `
+		-To $dest_mail `
+		-Subject $subject `
+		-Body $Body `
+		-Encoding 'UTF8' `
+		-SmtpServer $stmp_server -port 587 -UseSsl `
+		-Credential $Credential
+		break
+	}
+	else
+	{
+		sleep -Seconds 5
+	}
+}
